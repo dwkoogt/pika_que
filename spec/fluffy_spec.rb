71c85d743c750b303c29b2db5db32a85fd2f1581
Dong Wook Koo <dongwook@culinaryagents.com>
2018-01-16 16:45:02 -0500

    comment out default

--- spec/fluffy_spec.rb
+++ spec/fluffy_spec.rb
@@ -5,7 +5,7 @@ describe Fluffy do
     expect(Fluffy::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
+  # it 'does something useful' do
+  #   expect(false).to eq(true)
+  # end
 end
