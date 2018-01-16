b2915dc320c2c00a39b550be97c09dadf0e7e172
Dong Wook Koo <dongwook@culinaryagents.com>
2018-01-16 17:16:49 -0500

    string to class constantize

---
+++ lib/fluffy/util.rb
@@ -0,0 +1,17 @@
+module Fluffy
+  module Util
+    extend self
+    
+    def constantize(str)
+      return str if str.is_a? Class
+
+      names = str.split('::')
+      names.shift if names.empty? || names.first.empty?
+
+      names.inject(Object) do |constant, name|
+        constant.const_defined?(name) ? constant.const_get(name) : constant.const_missing(name)
+      end
+    end
+
+  end
+end
